10d6be8677147fab023947867aee0d0bc28c03b9
ruben4181 <ruben4181@gmail.com>
2019-03-08 13:36:57 -0500

    Nuevo tipo de adaptador

--- app/src/main/java/com/example/ruben4181/newsfeed/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/ruben4181/newsfeed/adapters/NewsAdapter.kt
@@ -4,9 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.ruben4181.newsfeed.Noticia
 import com.example.ruben4181.newsfeed.R
+
 
 class NewsAdapter(val items : ArrayList<Noticia>) : RecyclerView.Adapter<NewsAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
@@ -26,5 +28,6 @@ class NewsAdapter(val items : ArrayList<Noticia>) : RecyclerView.Adapter<NewsAda
     class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         val titleTV = itemView.findViewById<TextView>(R.id.card_news_title)
         val abstTV = itemView.findViewById<TextView>(R.id.card_new_abs)
+        val newsImage = itemView.findViewById<ImageView>(R.id.new_imageview)
     }
 }
